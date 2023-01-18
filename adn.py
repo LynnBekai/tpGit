1c468e6cec8d774aa07d87871aec3c7adf5d7ab5
Lynn Bekai <bekaliynn@hotmail.com>
2023-01-18 16:55:02 +0100

    essaie Olivier

---
+++ adn.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    documentation here
+"""
+
+__author__ = 'Lynn Bekai'
+
+
+
+def is_valid(adn_str):
+    pass
+
+
+def get_valid_adn(prompt='chaîne : '):
+    pass
